caa0adbfb1a07bc74c950cf466a43ddee5a00ff0
Poovarasan S <poovarasan936161@gmail.com>
2026-07-13 18:45:02 +0530

    feat: initialize project structure with icons, hero image, and base application files

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from "react";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-        <div className="mt-8 p-4 bg-purple-100  border border-purple-300  max-w-md mx-auto">
-          <p className="text-red-800 font-light text-sm">
-            Tailwind CSS v4 is successfully configured! 🎉
-          </p>
-        </div>
+      <div className="mt-8 p-4 bg-purple-100  border border-purple-300  max-w-md mx-auto">
+        <p className="text-red-800 font-light text-sm">
+          Tailwind CSS v4 is successfully configured! 🎉
+        </p>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
