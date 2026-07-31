ab768ceeb4fb2309e50e06028c07267160abff1f
Deepak5556 <deepakviji5556@gmail.com>
2026-07-31 22:01:23 +0530

    feat: initialize global CSS with custom styles and update Tailwind font configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,8 +11,9 @@ export default {
     extend: {
       colors: themeConfig.colors,
       fontFamily: {
-        sans: ['Inter', 'sans-serif'],
-        serif: ['Montserrat', 'sans-serif'],
+        // sans: ['"SF Pro Text"', '"SF Pro Display"', '-apple-system', 'BlinkMacSystemFont', 'Poppins', 'sans-serif'],
+        // serif: ['"SF Pro Display"', '"SF Pro Text"', '-apple-system', 'BlinkMacSystemFont', 'Poppins', 'sans-serif'],
+        poppins: ['Poppins', '"SF Pro Display"', '"SF Pro Text"', 'sans-serif'],
       },
       boxShadow: {
         'soft': '0 10px 40px -10px rgba(0,0,0,0.08)',
